9dc2c6a7597145dde564ba112c3479dbe4d6aa3c
Fernando Blat <fernando@blat.es>
2019-10-17 08:14:37 +0200

    Simplify

--- actions/deploy.rb
+++ actions/deploy.rb
@@ -1,10 +1,9 @@
 module Actions
   class Deploy
     def self.run(application_name, environment)
-      host = "https://deploy-bot.populate.tools"
-      path = "/api/deploys/#{application_name}/#{environment}"
+      url = "https://deploy-bot.populate.tools/api/deploys/#{application_name}/#{environment}"
 
-      response = HTTParty.post(host + path, {
+      response = HTTParty.post(url, {
         headers: {"Authorization" => "Token token=#{ENV["DEPLOY_BOT_TOKEN"]}"},
         debug_output: STDOUT,
       })
