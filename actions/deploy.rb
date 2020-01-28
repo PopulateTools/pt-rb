ee6e330f898892467ab9c029dc137a8a0673ec28
Fernando Blat <fernando@blat.es>
2020-01-28 11:37:11 +0100

    Gobierto data import

--- actions/deploy.rb
+++ actions/deploy.rb
@@ -1,11 +1,18 @@
 module Actions
   class Deploy
     def self.run(application_name, environment)
+      env_var = "DEPLOY_BOT_TOKEN"
+      if ENV[env_var].nil?
+        puts
+        puts "[ERROR] Please setup the environment variable #{env_var}"
+        puts
+        exit(-1)
+      end
       uri = URI.parse("https://deploy-bot.populate.tools/api/deploys/#{application_name}/#{environment}")
 
       https = Net::HTTP.new(uri.host, uri.port)
       https.use_ssl = true
-      request = Net::HTTP::Post.new(uri.path, initheader = {"Authorization" => "Token token=#{ENV["DEPLOY_BOT_TOKEN"]}"})
+      request = Net::HTTP::Post.new(uri.path, initheader = {"Authorization" => "Token token=#{ENV[env_var]}"})
       response = https.request(request)
       puts response.body
     end
---
+++ actions/gobierto_data_import.rb
@@ -0,0 +1,27 @@
+module Actions
+  class GobiertoDataImport
+    def self.run(args)
+      env_var = "GOBIERTO_DATA_ADMIN_TOKEN"
+      if ENV[env_var].nil?
+        puts
+        puts "[ERROR] Please setup the environment variable #{env_var}"
+        puts
+        exit(-1)
+      end
+
+      import_command = <<-COMMAND
+curl -X POST \
+     -H "Authorization: Bearer #{ENV[env_var]}" \
+     -H 'Content-Type: multipart/form-data' \
+     -F 'dataset[data_file]=@#{File.expand_path(args[3])}' \
+     -F 'dataset[schema_file]=@#{File.expand_path(args[4])}' \
+     -F 'dataset[name]=#{args[1]}' \
+     -F 'dataset[table_name]=#{args[2]}' \
+     -F 'dataset[csv_separator]=,' \
+     https://#{args[0]}/api/v1/data/datasets
+COMMAND
+
+      system(import_command)
+    end
+  end
+end
